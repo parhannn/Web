806d4aa08f5f91dd4fcce4b9537ccb42cfc3507e
parhannn <farhannn58@gmail.com>
2023-02-23 19:05:21 +0700

    Initial commit

---
+++ src/assets/main.js
@@ -0,0 +1,8 @@
+var username = prompt("Who's your name?")
+
+if (username == null) {
+    document.write("Hi NAME, Welcome To Website")
+}
+else{
+    document.write("Hi " + username + "!, Welcome To BajuKU.")
+}
